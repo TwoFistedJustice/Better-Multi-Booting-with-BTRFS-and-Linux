75c695d88673690879f81be34950f35f7c4c4d45
TwoFistedJustice <22874887+TwoFistedJustice@users.noreply.github.com>
2023-08-15 19:08:11 -0400

    feat: add script to rename subvolumes

---
+++ scripts/rename-btrfs-subvolume.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#rename-btrfs-subvolome.sh
+# usage: ./rename-btrfs-subvolome.sh @ @kubuntu
+# It mounts the BTRFS root subvolume
+# Then it looks for the subvolume name you pass as the first argument
+# If it finds it, it renames it to whatever you passed as the second argument.
+# it creates the folder /mnt/btrfsroot and later removes it
+
+# check number of args and display usage if wrong
+if (( $# != 2 ))
+  then
+    echo "usage: $0 [ old-subvolume-name ] new-subvolume-name"
+    exit 1;
+fi
+
+declare -r mountPoint="/mnt/btrfsroot/"
+declare -r device=$(findmnt -vno SOURCE /home/)
+
+#check if mount point exists
+#if it doesn't exist, create it
+if [[  ! -d $mountPoint  ]]
+  then
+    echo -e "Making $mountPoint"
+    sudo mkdir -p $mountPoint
+fi
+
+
+# mount btrfs-root
+# change directory to mount point
+sudo mount -o subvolid=5 $device $mountPoint
+echo -e "Changing directory to $mountPoint"
+pushd $mountPoint
+
+#rename subv $1 to $2
+if [[ -d $1 ]]
+  then
+    sudo mv $1 $2
+  else
+    echo -e "Did not find directory: $mountPoint$1"
+fi
+
+# unmount btrfs root
+umount $mountPoint
+
+# remove mount point
+sudo rmdir $mountPoint
+echo -e "Returning to original directory"
+popd
