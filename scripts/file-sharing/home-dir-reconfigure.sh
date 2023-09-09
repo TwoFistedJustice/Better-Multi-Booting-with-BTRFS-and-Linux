90e0a4b3da82c0fc9462f88d37991bef3f267ce4
Russ Bain <22874887+TwoFistedJustice@users.noreply.github.com>
2023-09-08 20:47:33 -0400

    feat: check if @data mounted, alert user if not

--- scripts/file-sharing/home-dir-reconfigure.sh
+++ scripts/file-sharing/home-dir-reconfigure.sh
@@ -6,10 +6,33 @@
 #  modify the script.
 # this will work on the current user only
 
+
+
+
 # var array of folder names
 declare -ar folders=("Documents" "Downloads" "Music" "Pictures" "Public" "Templates" "Videos")
 # var string path of data folder
 declare -r mountPoint="/mnt/data/"
+# var to capture user response and convert input to lowercase
+declare -l userResponse
+# Before this script executes
+# it should check to see if @data is mounted at /mnt/data
+
+
+# check if @data is mounted
+if  ! (grep '^\/dev\/' /proc/mounts | grep subvol=\/@data >> /dev/null)
+  then
+    echo -e "@data subvolume appears to not be mounted. It should be mounted in fstab before proceeding."
+    echo -e "If you would like to proceed anyway type the word 'yes'."
+    read userResponse
+      if [[ ! $userResponse == "yes" ]]
+      then
+        echo -e "Stopping operation."
+        exit 1
+      fi
+else
+    echo "It's not mounted."
+fi
 
 
 # cd to home/user,
@@ -18,6 +41,10 @@ cd ~/
 echo -e "results of pwd:"
 # display the current location
 pwd
+
+
+
+
 
 
 # iterate array
